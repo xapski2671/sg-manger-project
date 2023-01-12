6fb4b55a526845c461b331520f530d7c1fb97e61
xapski <xapski2671@gmail.com>
2023-01-12 06:11:11 +0100

    cmt0

--- src/crowd-funder.ts
+++ src/crowd-funder.ts
@@ -1,51 +1,42 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Address, BigInt, store, Bytes } from "@graphprotocol/graph-ts"
 import {
   CrowdFunder,
-  CampaignAdded,
-  CampaignCanceled
+  CampaignAdded as CampaignAddedEvent,
+  CampaignCanceled as CampaignCanceledEvent
 } from "../generated/CrowdFunder/CrowdFunder"
-import { ExampleEntity } from "../generated/schema"
+import { CampaignAdded, CampaignCanceled } from "../generated/schema"
 
-export function handleCampaignAdded(event: CampaignAdded): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+export function handleCampaignAdded(event: CampaignAddedEvent): void {
+  let id = event.params._campaignAddress.toHex()
+  let campaignAdded = CampaignAdded.load(id)
+  if(!campaignAdded){
+    campaignAdded = new CampaignAdded(id)
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  campaignAdded.campaignAddress = event.params._campaignAddress
+  campaignAdded.creator = event.params._creator
+  campaignAdded.creatorName = event.params._creatorName
+  campaignAdded.creatorType = event.params._creatorType
+  campaignAdded.description = event.params._description
+  campaignAdded.title = event.params._title
+  campaignAdded.tags = event.params._tags
+  campaignAdded.goalAmount = event.params._goalAmount
+  campaignAdded.duration = event.params._duration
+  campaignAdded.createdAt = event.block.timestamp
 
-  // Entity fields can be set based on event parameters
-  entity._campaignAddress = event.params._campaignAddress
-  entity._creator = event.params._creator
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.getCampaign(...)
+  campaignAdded.save()
 }
 
-export function handleCampaignCanceled(event: CampaignCanceled): void {}
+export function handleCampaignCanceled(event: CampaignCanceledEvent): void {
+  let id = event.params._campaignAddress.toHex()
+  let campaignCanceled = CampaignCanceled.load(id)
+  if(!campaignCanceled){
+    campaignCanceled = new CampaignCanceled(id)
+  }
+
+  campaignCanceled.campaignAddress = event.params._campaignAddress
+  campaignCanceled.createdAt = event.block.timestamp
+
+  store.remove("CampaignAdded", id)
+  campaignCanceled.save()
+}
